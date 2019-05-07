a164ca0e878fc6c1f1214d34c09cb86666289f29
Son VX <sonvx@cs.umu.se>
2019-05-07 19:38:32 +0200

    Update to add port to visualizer

--- src/codes/04.run_etnlp_visualizer_sbs.sh
+++ src/codes/04.run_etnlp_visualizer_sbs.sh
@@ -2,4 +2,4 @@
 export PYTHONPATH="$PYTHONPATH:$PWD"
 INPUT_FILES="../data/embedding_dicts/ELMO_23.vec;../data/embedding_dicts/FastText_23.vec;../data/embedding_dicts/W2V_C2V_23.vec;../data/embedding_dicts/MULTI_23.vec"
 # python ./visualizer/visualizer_sbs.py  -input $INPUT_FILES -args visualizer
-python ./visualizer/visualizer_sbs.py $INPUT_FILES
+python3 ./visualizer/visualizer_sbs.py $INPUT_FILES
