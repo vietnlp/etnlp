948d2ca1c574b2ff58b3141f93a92b9b14bb97e6
Son Vu <sonvx@cs.umu.se>
2019-05-07 19:40:44 +0200

    Add python3 to visualizer

--- src/codes/03.run_etnlp_visualizer_inter.sh
+++ src/codes/03.run_etnlp_visualizer_inter.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export PYTHONPATH="$PYTHONPATH:$PWD"
 INPUT_FILES="../data/embedding_dicts/ELMO_23.vec;../data/embedding_dicts/FastText_23.vec;../data/embedding_dicts/W2V_C2V_23.vec;../data/embedding_dicts/MULTI_23.vec"
-python ./etnlp_api.py  -input $INPUT_FILES -args visualizer -port 8889
+python3 ./etnlp_api.py  -input $INPUT_FILES -args visualizer -port 8889
